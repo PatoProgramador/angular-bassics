35ec50b7fa94ab36eb56c82734e058cd40327da8
PatoProgramador <skpates18@gmail.com>
2024-06-24 11:33:50 -0500

    Form module, form reactive

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
   selector: 'app-contact',
@@ -7,4 +8,32 @@ import { Component } from '@angular/core';
 })
 export class ContactComponent {
 
+  // partes del form plantilla
+  // public user: any = {
+  //   name: '',
+  //   email: ''
+  // }
+
+  // enviar() {
+  //   console.log(this.user);
+  // }
+
+  // form reactivo
+
+  formularioContacto: FormGroup;
+
+  constructor(private form: FormBuilder) {
+    this.formularioContacto = this.form.group({
+      nombre: ['', [Validators.required, Validators.minLength(3)]],
+      email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  hasErros( controlName: string, errorType: string) {
+    return this.formularioContacto.get(controlName)?.hasError(errorType) && this.formularioContacto.get(controlName)?.touched;
+  }
+
+  enviar() {
+    console.log(this.formularioContacto)
+  }
 }
