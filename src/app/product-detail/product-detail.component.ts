195ae40cb62c3fcab9998673acedb580fddff09a
PatoProgramador <skpates18@gmail.com>
2024-06-24 10:08:43 -0500

    Params setting on the detail

--- src/app/product-detail/product-detail.component.ts
+++ src/app/product-detail/product-detail.component.ts
@@ -1,10 +1,24 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
+import { ActivatedRoute } from '@angular/router';
 
 @Component({
   selector: 'app-product-detail',
   templateUrl: './product-detail.component.html',
   styleUrls: ['./product-detail.component.css']
 })
-export class ProductDetailComponent {
+export class ProductDetailComponent implements OnInit {
 
+  constructor(private _route: ActivatedRoute) {}
+
+  producto: string = '';
+  color: string = '';
+
+  ngOnInit(): void {
+    this._route.params.subscribe(
+      params => {
+        this.producto = params['productId'],
+        this.color = params['category']
+      }
+    );
+  }
 }
