69dec2637ffbfeff29f16ca94d21c41986b21c16
PatoProgramador <skpates18@gmail.com>
2024-06-24 13:38:10 -0500

    Reactive form and validations

--- src/app/contact/contact.component.ts
+++ src/app/contact/contact.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormBuilder, FormGroup, Validators } from '@angular/forms';
 
 @Component({
@@ -6,7 +6,7 @@ import { FormBuilder, FormGroup, Validators } from '@angular/forms';
   templateUrl: './contact.component.html',
   styleUrls: ['./contact.component.css']
 })
-export class ContactComponent {
+export class ContactComponent implements OnInit {
 
   // partes del form plantilla
   // public user: any = {
@@ -21,11 +21,43 @@ export class ContactComponent {
   // form reactivo
 
   formularioContacto: FormGroup;
+  tipoDni: string = 'DNI';
+  // ejemplo de usuario activo
+  // usuarioActivo: any = {
+  //   nombre: 'Pedro',
+  //   apellido: 'Perez',
+  //   dni: '12345678'
+  // };
 
   constructor(private form: FormBuilder) {
     this.formularioContacto = this.form.group({
       nombre: ['', [Validators.required, Validators.minLength(3)]],
+      apellido: ['', [Validators.required, Validators.minLength(3)]],
+      tipoDni: [''],
+      dni: ['', [Validators.required, Validators.minLength(3)]],
       email: ['', [Validators.required, Validators.email]]
+    })
+  }
+
+  ngOnInit(): void {
+    // ejemplo cuando queremos setear los datos del form a partir del usuario activo
+    // para actualizar y quitar los validators
+    // this.formularioContacto.get('apellido')?.clearValidators();
+    // this.formularioContacto.get('apellido')?.updateValueAndValidity();
+    // // actualizar solo una parte del form
+    // this.formularioContacto.patchValue({
+    //   nombre: this.usuarioActivo.nombre,
+    //   apellido: this.usuarioActivo.apellido,
+    //   dni: this.usuarioActivo.dni
+    // })
+    // // quitar los espacios llenos
+    // this.formularioContacto.get('nombre')?.disable()
+    // this.formularioContacto.get('apellido')?.disable()
+    // this.formularioContacto.get('dni')?.disable()
+
+    // para escuchar cada cambio que se haga en el formulario
+    this.formularioContacto.get('tipoDni')?.valueChanges.subscribe(value => {
+      this.tipoDni = value;
     })
   }
 
